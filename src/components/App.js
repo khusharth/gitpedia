a6d3b16f0d08f07f6aa07461baac247944f717b6
Khusharth <khusharth19@gmail.com>
2020-06-03 23:38:25 +0530

    Configured React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { GlobalStyle } from "../style";
+import { Router, Route, Switch } from "react-router-dom";
+import history from "../history";
+import Home from "../pages/Home";
+import UserProfile from "../pages/UserProfile";
 
 const App = () => {
-  return <div> App </div>;
+    return (
+        <>
+            <GlobalStyle />
+            <Router history={history}>
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/user/:id' component={UserProfile} />
+                </Switch>
+            </Router>
+        </>
+    );
 };
 
 export default App;
